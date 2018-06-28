7b35ee92dfbcfd9d5b3f7dd34e37c3edec31bd15
jaejin1 <opiximeo@gmail.com>
2018-06-28 13:33:26 +0900

    test

--- Future_Forecast/test.py
+++ Future_Forecast/test.py
@@ -21,26 +21,30 @@ while True:
     rows = curs.fetchall()
     print(rows)  # db result 
 
-    conn.close()
+    
 
     data_x = np.array([rows[i][0] for i in range(4)])
     data_y = np.array([rows[i][1] for i in range(4)])
+    #data_z = np.array([rows[i][2] for i in range(4)])
     data_x.astype('float32')
     data_y.astype('float32')
+    #data_z.astype('float32')
 
     print('===== data load =====')
 
     print(data_x)
     print(data_y)
+    print(data_z)
 
     test_x = np.reshape(data_x, (len(data_x),1))
     test_y = np.reshape(data_y, (len(data_y),1))
-
+    #test_z = np.reshape(data_z, (len(data_z),1))
 
     # normalizations
     scaler = MinMaxScaler(feature_range=(0, 1))
     nptf_x = scaler.fit_transform(test_x)
     nptf_y = scaler.fit_transform(test_y)
+    #nptf_z = scaler.fit_transform(test_z)
 
     #model.save('my_model.h5')
     model = load_model('my_model.h5')
@@ -51,6 +55,9 @@ while True:
 
     last_y = nptf_y[-1]
     last_y = np.reshape(last_y, (1, 1, 1))
+    
+    #last_z = nptf_z[-1]
+    #last_z = np.reshape(last_z, (1, 1, 1))
 
     print(last_x)
 
@@ -77,6 +84,8 @@ while True:
     print('Predict gyro_x fourth: %d' % lastX4)  
 
     if lastX4 > 10 or lastX4 < -10:
+        sql = 'update solar1 set safe1="1" where num>1 order by num desc limit 1'
+        curs.execute(sql)
         print('dangerous!!')
         # add sql
 
@@ -106,7 +115,42 @@ while True:
     print('Predict gyro_y fourth: %d' % lastY4)  
     
     if lastX4 > 10 or lastX4 < -10:
+        sql = 'update solar1 set safe1="1" where num>1 order by num desc limit 1'
+        curs.execute(sql)
         print('dangerous!!')
         # add sql
+        
+    '''
+    print(last_z)
 
+    lastZ = model.predict(last_z)
+    lastZ2 = np.reshape(lastZ, (1, 1, 1))
+
+    lastZ2 = model.predict(lastZ2)
+    lastZ3 = np.reshape(lastZ2, (1, 1, 1))
+
+    lastZ3 = model.predict(lastZ3)
+    lastZ4 = np.reshape(lastZ3, (1, 1, 1))
+
+    lastZ4 = model.predict(lastZ4)
+
+
+    lastZ = scaler.inverse_transform(lastZ)
+    lastZ2 = scaler.inverse_transform(lastZ2)
+    lastZ3 = scaler.inverse_transform(lastZ3)
+    lastZ4 = scaler.inverse_transform(lastZ4)
+
+    print('Predict gyro_x first: %d' % lastZ)  
+    print('Predict gyro_x second: %d' % lastZ2)  
+    print('Predict gyro_x third: %d' % lastZ3)  
+    print('Predict gyro_x fourth: %d' % lastZ4)  
+
+    if lastX4 > 10 or lastX4 < -10:
+        sql = 'update solar1 set safe1="1" where num>1 order by num desc limit 1'
+        curs.execute(sql)
+        print('dangerous!!')
+        # add sql
+    '''
+    
+    conn.close()
     time.sleep(5)
